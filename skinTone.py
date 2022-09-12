115f14ba695d96af4bc966e4440eb97c031e290d
kwonikhyeon <41859012+kwonikhyeon@users.noreply.github.com>
2022-09-13 00:28:46 +0900

    Update skinTone.py

--- skinTone.py
+++ skinTone.py
@@ -12,6 +12,61 @@ import cv2
 from numpy.core.fromnumeric import size
 from matplotlib import pyplot as plt
 
+def display_image(image, name):
+    window_name = name
+    cv2.namedWindow(window_name)
+    cv2.imshow(window_name, image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+# segment using otsu binarization and thresholding
+def segment_otsu(image_grayscale, img_BGR):
+    threshold_value, threshold_image = cv2.threshold(image_grayscale, 0, 255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+    display_image(threshold_image, "otsu") 
+    threshold_image_binary = 1- threshold_image/255
+    threshold_image_binary = np.repeat(threshold_image_binary[:, :, np.newaxis], 3, axis=2)
+    img_face_only = np.multiply(img_BGR, threshold_image_binary)
+    return img_face_only
+
+
+def global_skinToneDetect(img):
+    image_path = './crop dataset/all_N_001.png'
+    img_BGR = img
+
+    img_face_only = img_BGR
+    img_HSV = cv2.cvtColor(img_face_only, cv2.COLOR_BGR2HSV)
+    img_YCrCb = cv2.cvtColor(img_face_only, cv2.COLOR_BGR2YCrCb)
+    display_image(img_HSV, "HSV")
+    display_image(img_YCrCb, "YCrCb")
+
+    # aggregate skin pixels
+    blue = []
+    green = []
+    red = []
+
+    height, width, channels = img_face_only.shape
+
+    for i in range (height):
+        for j in range (width):
+            if((img_HSV.item(i, j, 0) <= 170) and (140 <= img_YCrCb.item(i, j, 1) <= 170) and (90 <= img_YCrCb.item(i, j, 2) <= 120)):
+                blue.append(img_face_only[i, j].item(0))
+                green.append(img_face_only[i, j].item(1))
+                red.append(img_face_only[i, j].item(2))
+            else:
+                img_face_only[i, j] = [0, 0, 0]
+
+    display_image(img_face_only, "final segmentation")
+
+    # determine mean skin tone estimate
+    skin_tone_estimate_BGR = [np.mean(blue), np.mean(green), np.mean(red)]
+    avr_color = cv2.merge([np.full((560,560), skin_tone_estimate_BGR[0], dtype=np.uint8),
+                np.full((560,560), skin_tone_estimate_BGR[1], dtype=np.uint8),
+                np.full((560,560), skin_tone_estimate_BGR[2], dtype=np.uint8)])
+    display_image(avr_color, "avr")
+    print("mean skin tone estimate (BGR)", skin_tone_estimate_BGR[0], skin_tone_estimate_BGR[1], skin_tone_estimate_BGR[2], "]")
+    return skin_tone_estimate_BGR
+
 def skinToneDetect(data_dir, imgName):
 
     # 피부톤 컬러차트
@@ -73,10 +128,12 @@ def skinToneDetect(data_dir, imgName):
 
 
 
-if __name__ == '__main__':
-    data_dir = './crop dataset'
-    imgName = 'all_N_221.png'
-    skinToneDetect(data_dir, imgName)
-    skinToneDetect(data_dir, 'all_white.jpg')
-    skinToneDetect(data_dir, 'all_black.jpg')
+
+
+# if __name__ == '__main__':
+#     data_dir = './crop dataset'
+#     imgName = 'all_N_221.png'
+#     skinToneDetect(data_dir, imgName)
+#     skinToneDetect(data_dir, 'all_white.jpg')
+#     skinToneDetect(data_dir, 'all_black.jpg')
     
